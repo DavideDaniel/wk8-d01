2a1bf7a94887ab93c6961e71975372e56038ae54
DavideDaniel <davidedaniel@gmail.com>
2015-01-21 23:36:37 -0500

    update

--- httParty/server.rb
+++ httParty/server.rb
@@ -4,3 +4,19 @@ require "pry"
 url = "http://api.randomuser.me"
 response = HTTParty.get(url)
 binding.pry
+
+<script>
+    
+// var button = document.querySelector("button")
+//       button.addEventListener("click", function(){
+//       var city = document.querySelector("#city")
+
+//     var mapOptions = {
+//         center: { city: city.value },
+//         zoom: 9
+//       };
+//       var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    
+//       console.log("button");
+//       })
+    </script>
